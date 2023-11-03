31572cbe08549f965c28fdb484596aa3d7a621ac
Laminhdatau <Laminhdataulamin2@gmail.com>
2023-11-03 13:13:32 +0800

    [+] registrasi

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-
+var morgan = require("morgan");
 const app = express();
 
 //parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(morgan("dev"));
 
 //panggil routes
 var routes = require("./routes");
 routes(app);
+
+//MENU ROUTES FROM INDEX
+
+app.use("/auth", require("./middleware"));
 
 app.listen(3000, () => {
   console.log("Sukses");
